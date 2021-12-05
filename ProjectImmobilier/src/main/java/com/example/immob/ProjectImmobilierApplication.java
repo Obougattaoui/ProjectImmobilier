05c8be9d85fafd142744838f5701aad5b047d17f
OBougatt <oussama10.ob33@gmail.com>
2021-12-06 00:20:22 +0100

    create packages and classes VERSION1

---
+++ ProjectImmobilier/src/main/java/com/example/immob/ProjectImmobilierApplication.java
@@ -0,0 +1,13 @@
+package com.example.immob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectImmobilierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectImmobilierApplication.class, args);
+	}
+
+}
